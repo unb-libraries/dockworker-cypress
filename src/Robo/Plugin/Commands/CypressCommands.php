1347f0cdd8071b02d665d206bc5acf68977c45c4
Patrick Schilf <pschilf@unb.ca>
2022-11-30 14:28:31 -0400

    Add cypress:up command

--- src/Robo/Plugin/Commands/CypressCommands.php
+++ src/Robo/Plugin/Commands/CypressCommands.php
@@ -39,11 +39,37 @@ class CypressCommands extends DockworkerCommands {
    * @throws \Dockworker\DockworkerException
    */
   public function runCypressTests() {
-    $this->io()->title("Running Cypress Tests");
-    $this->taskDockerStart($this->getContainerId())
-      ->run();
+    $this->setRunOtherCommand('cypress:up');
     $this->taskDockerLogs($this->getContainerId(), '-f', '-n 0')
       ->run();
+  }
+
+  /**
+   * Start the cypress container.
+   *
+   * @command cypress:up
+   *
+   * @option string|null $service
+   *   Name of the cypress container. Defaults to the name of the primary
+   *   service container, prefixed by "cypress".
+   * @option bool $forceRecreate
+   *   Same as docker-compose's --force-recreate.
+   * @option bool $noDeps
+   *   Same as docker-compose's --no-deps.
+   */
+  public function up($service = 'cypress', bool $forceRecreate = FALSE, bool $noDeps = FALSE) {
+    $this->io()->title("Starting cypress container");
+
+    $cmd = 'docker-compose up -d';
+    if ($forceRecreate) {
+      $cmd .= ' --force-recreate';
+    }
+    if ($noDeps) {
+      $cmd .= ' --no-deps';
+    }
+    $cmd .= $service;
+
+    $this->_exec($cmd);
   }
 
 }
